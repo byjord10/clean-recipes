09e92478f10406a60eb8c6974574a6b16cfa03fe
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-29 00:37:26 +0000

    first commit

---
+++ lib/clean/recipes.rb
@@ -0,0 +1,8 @@
+require "clean/recipes/version"
+
+module Clean
+  module Recipes
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/clean/recipes/version.rb
@@ -0,0 +1,5 @@
+module Clean
+  module Recipes
+    VERSION = "0.1.0"
+  end
+end
